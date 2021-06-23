e59cde744c7dcac8b92dcb8f10fae5908b781ace
MarkBilginer <markbilginer@gmail.com>
2021-06-23 15:15:24 +0200

    basic_madlib; input(), format string, print()

---
+++ src/madlibs.py
@@ -0,0 +1,10 @@
+adj = input("Adjective: ")
+verb1 = input("Verb 1: ")
+verb2 = input("Verb 2: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Philosophy is so {adj}! It makes me excited all the time because " \
+ f"I love to {verb1}. Exercise your mind and {verb2} like you are " \
+ f"{famous_person}"
+
+print(madlib)
