391b6b652882be9cf089ab906f45041d9024a70b
MarkBilginer <markbilginer@gmail.com>
2021-06-23 22:02:03 +0200

    Implemented alternative version og computer guesses the number, where user directs the opponent with H, L and C commands, respectively for too high, too log, and correct. Optional fix: if the user forgets the number and misdirects the computer, the program fails.

--- src/guess_number.py
+++ src/guess_number.py
@@ -51,9 +51,26 @@ def computer_guess(x: int) -> None:
 	else:
 		print(f"The computer guesses the number correctly: {guess}")
 
+def computer_guess_feedback(x: int) -> None:
+	low = 1
+	high = x
+	feedback = ''
+
+	while feedback != 'C' and low != high:
+		guess = randint(low, high)
+		feedback = input(f"Is {guess} too high (H), too low (L), or correct (C)?")
+		if feedback == 'H':
+			high = guess - 1
+		elif feedback == 'L':
+			low = guess + 1
+	if feedback == 'C':
+		print(f"The computer guessed your number, {guess}, correctly!")
+	else:
+		print("The computer couldn't guess your number. What happened??")
 
 if __name__ == "__main__":
 	print("Welcome to the boring guess the number game!")
 	# guess(10)
-	computer_guess(10)
+	# computer_guess(10)
+	computer_guess_feedback(1000)
 	print("Exiting...")
